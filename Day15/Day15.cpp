542b248bddf29219e825bc456a957077acc2d905
PopaRobert <95902235+PopaRobert@users.noreply.github.com>
2021-12-14 22:55:05 -0800

    Update Day15.cpp

--- Day15/Day15.cpp
+++ Day15/Day15.cpp
@@ -3,29 +3,14 @@
 #include <map>
 #include <algorithm>
 #include <fstream>
+#include <limits.h>
 
 using namespace std;
 
 #define ROW 500
 #define COL 500
 
-struct cell{
-    int x, y;
-    int distance;
-    cell(int x, int y, int distance): x(x), y(y), distance(distance) {}
-};
-
-bool operator < (const cell& a, const cell& b){
-    if (a.distance == b.distance){
-        if (a.x != b.x)
-            return (a.x < b.x);
-        else
-            return (a.y < b.y);
-    }
-    return (a.distance < b.distance);
-}
-
-bool isInsideGrid(int i, int j){
+bool inGrid(int i, int j){
     return (i >= 0 && i < ROW && j >= 0 && j < COL);
 }
 
@@ -36,23 +21,36 @@ int shortest(int grid[ROW][COL], int row, int col){
             dis[i][j] = INT_MAX;
     int dx[] = {-1, 0, 1, 0};
     int dy[] = {0, 1, 0, -1};
-    set<cell> st;
-    st.insert(cell(0, 0, 0));
+    set<pair<int, pair<int, int>>> st;
+    pair<int, pair<int, int>> hp;
+    hp.first = 0;
+    hp.second.first = 0;
+    hp.second.second = 0;
+    st.insert(hp);
     dis[0][0] = grid[0][0];
 
     while (!st.empty()){
-        cell k = *st.begin();
+        pair<int, pair<int, int>> k = *st.begin();
         st.erase(st.begin());
         for (int i = 0; i < 4; i++){
-            int x = k.x + dx[i];
-            int y = k.y + dy[i];
-            if (!isInsideGrid(x, y))
+            int x = k.second.first + dx[i];
+            int y = k.second.second + dy[i];
+            if (!inGrid(x, y))
                 continue;
-            if (dis[x][y] > dis[k.x][k.y] + grid[x][y]){
-                if (dis[x][y] != INT_MAX)
-                    st.erase(st.find(cell(x, y, dis[x][y])));
-                dis[x][y] = dis[k.x][k.y] + grid[x][y];
-                st.insert(cell(x, y, dis[x][y]));
+            if (dis[x][y] > dis[k.second.first][k.second.second] + grid[x][y]){
+                if (dis[x][y] != INT_MAX){
+                    pair<int, pair<int, int>> hp;
+                    hp.second.first = x;
+                    hp.second.second = y;
+                    hp.first = dis[x][y];
+                    st.erase(st.find(hp));
+                }
+                dis[x][y] = dis[k.second.first][k.second.second] + grid[x][y];
+                pair<int, pair<int, int>> hp;
+                hp.second.first = x;
+                hp.second.second = y;
+                hp.first = dis[x][y];
+                st.insert(hp);
             }
         }
     }
